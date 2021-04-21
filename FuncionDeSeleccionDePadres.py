24c3187ac12cb3c38a334045a7b362bdf3c43ab3
CeciCL <zS17013375@estudiantes.uv.mx>
2021-04-21 17:07:09 -0500

    Se cambió el método de selección de padres al método por ruleta(ponderación de rango)

--- FuncionDeSeleccionDePadres.py
+++ FuncionDeSeleccionDePadres.py
@@ -1,19 +1,39 @@
 from FuncionDeAptitud import fitness
 import random
 
-def SeleccionDePadres(poblacion_1):
-    poblacion_de_papas = []
-    while len(poblacion_de_papas) < len(poblacion_1):
-        i = random.randrange(0, len(poblacion_1))
-        mejor_papa = []
-        j = -1
-        while j < i:
-            papa_1 = poblacion_1[random.randrange(0, len(poblacion_1))]
-            papa_2 = poblacion_1[random.randrange(0, len(poblacion_1))]
-            if fitness(papa_2) > fitness(papa_1):
-                mejor_papa = papa_1
-            else:
-                mejor_papa = papa_2
-            j += 1
-        poblacion_de_papas.append(mejor_papa)
-    return poblacion_de_papas
+def SeleccionDePadres(poblacion):
+    """
+    Ordenar la población de menor a mayor fiteness con la función sorted
+    """
+    poblacion_en_orden = sorted(poblacion, key=fitness)
+    """
+    Calcular la probabilidad de ser selccionado y probabilidad acumulada
+    """
+    prob_de_seleccion = []
+    prob_acumulada = []
+    for i in range(1, len(poblacion) + 1):
+        p_pos = (len(poblacion) - i + 1) / ((len(poblacion) * (len(poblacion) + 1)) / 2)
+        prob_de_seleccion.append(p_pos)
+        p_acum = sum(prob_de_seleccion)
+        prob_acumulada.append(p_acum)
+    """
+    Generar dos números random y selelcionar los padres segun la probabilidad acumulada
+    """
+    poblacion_de_padres = []
+    while len(poblacion_de_padres) < int(len(poblacion) / 2):
+        par_de_padres = []
+        r1 = random.random()
+        r2 = random.random()
+        k1 = 0
+        k2 = 0
+        while prob_acumulada[k1] < r1:
+            k1 += 1
+        while prob_acumulada[k2] < r2:
+            k2 += 1
+        padre_1 = poblacion_en_orden[k1]
+        padre_2 = poblacion_en_orden[k2]
+        par_de_padres.append(padre_1)
+        par_de_padres.append(padre_2)
+        poblacion_de_padres.append(par_de_padres)
+
+    return poblacion_de_padres
