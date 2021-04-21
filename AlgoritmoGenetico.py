467162e3b91379024fcc1e785c87d3cb837cca0c
CeciCL <zS17013375@estudiantes.uv.mx>
2021-04-21 17:11:26 -0500

    Se arregló la parte de mejor fitness por población y peor fitness por población

--- AlgoritmoGenetico.py
+++ AlgoritmoGenetico.py
@@ -10,63 +10,53 @@ import matplotlib.pyplot as plt
 print('Hola,bienvenido a mi algoritmo génetico,aquí vamos a ver el problema de 0 colisiones entre reinas en un tablero de ajedrez.\n'
       'Lo primero que vamos a hacer es crear nuestra población,para eso voy a necesitar tú ayuda,pues vas a elegir el número de individuos que va a tener la población,dicho número tiene que ser par.')
 cant=int(input('Por favor ingresa el número de individuos que deseas que tenga la población.'))
-if cant%2 == 0:
-    poblacion_inicial = poblacion(8,cant)
-    mejor_individuo_por_generacion = []
-    peor_individuo_por_generacion = []
-    individuo_promedio_por_generacion = []
-    desviacion_estandar = []
-    generaciones = []
 
-    hijos = []
-    poblacion_de_padres = []
-    poblacion_de_mutados = []
-    generaciones = []
-    #Estp es para hacer las generaciones
-    for i in range(50):
-        #Selección de padres
-        poblacion_de_padres = SeleccionDePadres(poblacion_inicial)
-        #Cruza
-        poblacion_de_hijos = cruza(poblacion_de_padres)
-        #Mutación
-        poblacion_de_mutados = mutacion(poblacion_de_hijos)
-        #Reemplazo
-        poblacion_inicial = poblacion_de_mutados
-        generaciones.append(poblacion_inicial)
+poblacion_inicial = poblacion(8,cant)
+mejor_individuo_por_generacion = []
+peor_individuo_por_generacion = []
+individuo_promedio_por_generacion = []
+desviacion_estandar = []
+generaciones = []
 
-        #Número de mejores individuos por generacion
-        fitness_poblacion = []
-        for k1 in range(len(poblacion_inicial)):
-            fitness_indivicual = fitness(poblacion_inicial[k1])
-            fitness_poblacion.append(fitness_indivicual)
-        fitness_mas_pequeño = min(fitness_poblacion)
-        numero_de_individuos_con_mejor_fitness = 0
-        for j in range(len(poblacion_inicial)):
-            if fitness_mas_pequeño == fitness_poblacion[j]:
-                numero_de_individuos_con_mejor_fitness += 1
-        mejor_individuo_por_generacion.append(numero_de_individuos_con_mejor_fitness)
+hijos = []
+poblacion_de_padres = []
+poblacion_de_mutados = []
+generaciones = []
+#Esto es para hacer las generaciones
+for i in range(50):
+    #Selección de padres
+    poblacion_de_padres = SeleccionDePadres(poblacion_inicial)
+    #Cruza
+    poblacion_de_hijos = cruza(poblacion_de_padres)
+    #Mutación
+    poblacion_de_mutados = mutacion(poblacion_de_hijos)
+    #Reemplazo
+    poblacion_inicial = poblacion_de_mutados
+    generaciones.append(poblacion_inicial)
 
-        #Número de peores individuos por generación
-        fitness_mas_grande = max(fitness_poblacion)
-        numero_de_individuos_con_peor_fitness = 0
-        for j in range(len(poblacion_inicial)):
-            if fitness_mas_grande == fitness_poblacion[j]:
-                numero_de_individuos_con_peor_fitness += 1
-        peor_individuo_por_generacion.append(numero_de_individuos_con_peor_fitness)
+    #Fitness del mejor individuo por generacion
+    fitness_poblacion = []
+    for k1 in range(len(poblacion_inicial)):
+        fitness_indivicual = fitness(poblacion_inicial[k1])
+        fitness_poblacion.append(fitness_indivicual)
+    fitness_min = min(fitness_poblacion)
+    mejor_individuo_por_generacion.append(fitness_min)
 
-        #Número de individuos promedio
-        promedio = sum(fitness_poblacion)/len(fitness_poblacion)
-        individuo_promedio_por_generacion.append(promedio)
+    #Fitness del peores individuo por generación
+    fitness_mas_grande = max(fitness_poblacion)
+    peor_individuo_por_generacion.append(fitness_mas_grande)
 
-        #Desviacion estandar
-        suma = 0
-        for i in range(len(fitness_poblacion)):
-            desv = (fitness_poblacion[i] - promedio) ** 2
-            suma += desv
-        varianza = suma / (len(fitness_poblacion))
-        desviacion_estandar.append(np.sqrt(varianza))
-else:
-    cant =print('El número que ingresaste no es valido,por favor intenta de nuevo.\n')
+    #Número de individuos promedio
+    promedio = sum(fitness_poblacion)/len(fitness_poblacion)
+    individuo_promedio_por_generacion.append(promedio)
+
+    #Desviacion estandar
+    suma = 0
+    for i in range(len(fitness_poblacion)):
+        desv = (fitness_poblacion[i] - promedio) ** 2
+        suma += desv
+    varianza = suma / (len(fitness_poblacion))
+    desviacion_estandar.append(np.sqrt(varianza))
 
 #Esto es para hacer la tabla
 indices = range(50)
