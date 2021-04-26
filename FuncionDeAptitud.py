e1bfa20d5dd92cd1c68e6eec19f8f5823218e841
CeciCL <zS17013375@estudiantes.uv.mx>
2021-04-25 22:00:46 -0500

    Se hizo una revisión general.

--- FuncionDeAptitud.py
+++ FuncionDeAptitud.py
@@ -3,7 +3,7 @@ def fitness(vector):
     dimension = len(vector)
     for i in range(dimension):
         k_1 = vector[i]
-        for j in range(i+1,dimension):
+        for j in range(i+1, dimension):
             k_2 = vector[j]
             pen = (k_2-k_1)/(j-i)
             if pen == 1 or pen == (-1):
